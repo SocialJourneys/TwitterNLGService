b5b06c635d8219d5b408c3a045c60c4ed01eb44b
mmehdi <mmehdi@outlook.com>
2015-04-09 18:41:22 +0100

    post json

--- src/com/twitternlg/nlg/ATweet.java
+++ src/com/twitternlg/nlg/ATweet.java
@@ -10,4 +10,21 @@ public class ATweet {
 	private String date_start;
 	private String date_end;
 	private String service_status;
+	
+	public void print(){
+		System.out.println("event: "+ event);
+		System.out.println("bus_services: "+ bus_services);
+		System.out.println("problem: "+ problem);
+		System.out.println("location: "+ location);
+		System.out.println("diversion_road: "+ diversion_road);
+		System.out.println("duration: "+ duration);
+		System.out.println("date_start: "+ date_start);
+		System.out.println("date_end: "+ date_end);
+		System.out.println("service_status: "+ service_status);
+	}
+
+	public String getEvent(){
+		return this.event;
+	}
 }
+
--- src/com/twitternlg/servlet/service.java
+++ src/com/twitternlg/servlet/service.java
@@ -1,8 +1,11 @@
 package com.twitternlg.servlet;
 
+import com.twitternlg.nlg.ATweet;
 import com.twitternlg.nlg.TweetFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -10,6 +13,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 
 /**
@@ -47,19 +52,37 @@ public class service extends HttpServlet {
     	String date_end = request.getParameter("date_end");
     	String service_status = request.getParameter("service_status");
     	
-    	out.println(t.generateTweetString(null));
+    	//out.println(t.generateTweetString(null));
 
 	}
-
+    
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+	    // 1. get received JSON data from request
+	    BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+	    String json = "";
+	    if(br != null){
+	        json = br.readLine();
+	    }
+
+	    // 2. initiate jackson mapper
+	    ObjectMapper mapper = new ObjectMapper();
+
+	    // 3. Convert received JSON to Article
+	    ATweet tweet = mapper.readValue(json, ATweet.class);
+
+	    // 4. Set response type to JSON
+	    response.setContentType("application/json");            
+
 		PrintWriter out = response.getWriter();
+    	
+		out.println(tweet.getEvent());
 
-    	out.println("hello from server");
-
-		// TODO Auto-generated method stub
+	    // 6. Send List<Article> as JSON to client
+	   // mapper.writeValue(response.getOutputStream(), articles);
 	}
 
 }
